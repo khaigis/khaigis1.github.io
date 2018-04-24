b892870a9b73ddc5db7a88907e7fb06ce9d0c45a
khaigis <38685941+khaigis@users.noreply.github.com>
2018-04-24 11:51:09 +0300

    Add files via upload

---
+++ start.php
@@ -0,0 +1,23 @@
+<?PHP
+
+$name='Lisa';
+?>
+
+<div style="width: 1000px; height: 1000px; color: red;">
+
+<?PHP
+echo "{$name} has already been playing football for 3 hour";
+?>
+</div>
+
+
+
+<br></br>
+
+
+<script language="php">
+$a=$b=7;
+$name='Ann';
+echo "{$name} plays chess every day";
+
+</script>
